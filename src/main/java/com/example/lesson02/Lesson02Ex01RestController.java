450969825c48f9504966357e220ca6b785a6d1a7
학원 <test@test.com>
2023-09-20 21:45:06 +0900

    2강 예제 1

---
+++ src/main/java/com/example/lesson02/Lesson02Ex01RestController.java
@@ -0,0 +1,19 @@
+package com.example.lesson02;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson02.domain.UsedGoods;
+
+@RestController // @Controller + @ResponseBody
+public class Lesson02Ex01RestController {
+	
+	// 요청 URL : http://localhost:8080/lesson02/ex01
+	@RequestMapping("/lesson02/ex01")
+	public List<UsedGoods> ex01(){
+		List<UsedGoods> usedGoodsList = ;
+		return usedGoodsList; // json으로 view
+	}
+}
---
+++ src/main/java/com/example/lesson02/domain/UsedGoods.java
@@ -0,0 +1,69 @@
+package com.example.lesson02.domain;
+
+import java.util.Date;
+
+// DTO, model, domain
+public class UsedGoods {
+	// 필드
+	private int id;
+	private String title;
+	private int price;
+	private String description;
+	private String image;
+	private int sellerId;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	// 메소드
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public int getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+}
